b8c1aba612a2fdb213594d414077c698c5b91e8b
edoka <peter.edoka.242464@unn.edu.ng>
2022-07-01 06:34:39 -0700

    added scripts to out datatype size in memory

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - function to display the sizes of data types
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+	
+	printf("Size of a char: %lu byte(s)\n", sizeof(i));
+	printf("Size of a int: %lu byte(s)\n", sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(k));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", sizeof(m));
+
+	return (0);
+}
