c5a3d9acd2e85a505fe05918ecbeb4b4dc4f3074
edoka <peter.edoka.242464@unn.edu.ng>
2022-07-04 02:31:35 -0700

    printing digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits fro 0-9 using putchar function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <=10; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
