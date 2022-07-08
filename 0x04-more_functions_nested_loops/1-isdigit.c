c212f9acd9917a3a8e0c63666821ff09ba17b374
edoka <peter.edoka.242464@unn.edu.ng>
2022-07-08 14:45:38 -0700

    check ifdigit is in [0 to 9]

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check for a digit from 0 to 9
+ * @c: digit to enumerate
+ * Return: 1 if c is a digt, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
