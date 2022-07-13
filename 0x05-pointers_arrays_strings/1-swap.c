ff1194ccd7c54f5ac022b3bb3d0cb999bf099ee2
edoka <peter.edoka.242464@unn.edu.ng>
2022-07-12 18:34:36 -0700

    done with task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function that takes two int pointers as parameters and 
+ * and swap their values
+ *
+ * @a: pointer to first int
+ * @b: pointer to second int
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
