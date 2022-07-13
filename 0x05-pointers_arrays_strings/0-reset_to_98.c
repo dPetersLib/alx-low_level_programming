59a10893f30532b3fe4f90c769436f5a0a68d088
edoka <peter.edoka.242464@unn.edu.ng>
2022-07-12 17:47:07 -0700

    debug 2 for reset task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * reset_to_98 - resets an int to 98
  * main - check the code
- * @*n: pointer to the int
+ * 
  * Return: always 0
  */
-
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
-
 int main(void)
 {
 	int n;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-#include <stdio.h>
-#include <stdlib.h>
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
 
 #endif // MAIN_H_INCLUDED
