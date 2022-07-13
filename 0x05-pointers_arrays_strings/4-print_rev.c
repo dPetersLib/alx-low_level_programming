02fbb01196dd2cf6b112fda82601aa9e8ecbd0ad
edoka <peter.edoka.242464@unn.edu.ng>
2022-07-13 10:56:06 -0700

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,11 @@ void print_rev(char *s)
 {
 	int index;
 	
-	for (index =0; s[index] !- '\0'; index++)
+	for (index =0; s[index] != '\0'; index++)
 		;
 	for (index = index - 1; s[index] != '\0'; index--)
 	{
-		_putchar(s[index]);
+		putchar(s[index]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
