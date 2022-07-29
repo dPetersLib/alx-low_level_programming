0688da0ce2b279eb6baae85500371bdec2782d93
edoka <peter.edoka.242464@unn.edu.ng>
2022-07-29 10:16:08 -0700

    tast string_nconcat

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * malloc_checked - a function that allocates memory using malloc
  * @b: sizee of memory to be allocated
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,7 +4,11 @@
 void _puts(char *str);
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
-
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif
